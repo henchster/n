7458d4d8e42bac381432f81bab8e673d3b2c6863
Edmund Fong <henchster@gmail.com>
2011-06-01 20:14:28 +0800

    updates remove AT

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 N::Application.routes.draw do
 
   resources :supports, :only => [:new, :create]
-  get "home/index"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,7 @@ N::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "mobile#index"
 
   # See how all your routes lay out with "rake routes"
 
